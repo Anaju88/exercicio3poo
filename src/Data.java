5bf9e2d38bb4a87d2ae4af0d5ecf574fa378b4e8
anaju88 <anajoliveira888@gmail.com>
2025-03-22 18:05:52 -0300

    exercicio 2 da atividade 3 de poo

--- src/Data.java
+++ src/Data.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class Data {
+
+    public int dia;
+    public int mes;
+    public int ano;
+
+    public Data() {
+    }
+
+    public Data(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+    int cadastraDados(){
+        return dia;
+    }
+
 }
